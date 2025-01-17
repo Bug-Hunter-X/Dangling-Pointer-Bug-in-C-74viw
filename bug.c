58075cc3b02fcd84b7fec8340320a6786b1b03eb
BugHunter-X <askzenai@gmail.com>
2025-01-17 01:06:05 +0100

    Add bug.c file

---
+++ bug.c
@@ -0,0 +1,13 @@
+int main() {
+  int arr[5] = {1, 2, 3, 4, 5};
+  int *ptr = (int *)malloc(sizeof(int) * 5);
+  memcpy(ptr, arr, sizeof(int) * 5);
+
+  free(ptr);
+  ptr = NULL;
+
+  for (int i = 0; i < 5; i++) {
+    printf("%d ", arr[i]);
+  }
+  return 0;
+}
